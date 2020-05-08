e5877df5409555db8d9eee894eafe8cc82b0a681
CarloCori <57438555+CarloCori@users.noreply.github.com>
2020-05-08 13:00:34 +0200

    Update test_Excercise_1_Fibonacci.py

--- test_Excercise_1_Fibonacci.py
+++ test_Excercise_1_Fibonacci.py
@@ -9,14 +9,11 @@ from test_Excercise_1_Fibonacci_1 import fibonacci_number
 from test_Excercise_1_Fibonacci_1 import fibonacci_series_to
 from test_Excercise_1_Fibonacci_1 import fibonacci_series_from_to
 
-
 def test_fibonacci_number(n):
     fibonacci_number(6) == 5
    
 def test_fibonacci_series_to(n):
     fibonacci_series_to(3) == [0, 1, 1]
-
-
 
 def test_fibonacci_series_from_to(m, n):
     fibonacci_series_from_to(2, 4) == [1, 1, 2]
