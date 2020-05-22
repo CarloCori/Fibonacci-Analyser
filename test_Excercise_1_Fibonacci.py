c7371b7f23e98843c5a0cdd0bb2993dd5d5c2bfb
stickler-ci <support@stickler-ci.com>
2020-05-22 13:14:44 +0000

    Fixing style errors.

--- test_Excercise_1_Fibonacci.py
+++ test_Excercise_1_Fibonacci.py
@@ -2,12 +2,14 @@ from Excercise_1_Fibonacci import fibonacci_number
 from Excercise_1_Fibonacci import fibonacci_series_to
 from Excercise_1_Fibonacci import fibonacci_series_from_to
 
+
 def test_fibonacci_number():
     assert fibonacci_number(6) == 5
-   
+
+
 def test_fibonacci_series_to():
     assert fibonacci_series_to(3) == [0, 1, 2]
-    
+
 
 def test_fibonacci_series_from_to():
     assert fibonacci_series_from_to(2, 4) == [1, 1, 2]
